b38ad149e0e2bab49bc23e9a2ef2c2aff047cc4b
Eason12030105 <eason12030105@gmail.com>
2022-12-24 22:05:10 +0800

    4.4-批次爬取Jason格式的網頁資料(Post)

---
+++ Taifex_scrape.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+url = 'https://mis.taifex.com.tw/futures/api/getQuoteList'
+
+payload = {
+    "MarketType":"0",
+    "SymbolType":"F",
+    "KindID":"1","CID":"TXF",
+    "ExpireMonth":"",
+    "RowSize":"全部",
+    "PageNo":"",
+    "SortColumn":"",
+    "AscDesc":"A"}
+
+
+header = {'content-type': 'application/json',
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+}
+
+response = requests.post(url,data = json.dumps(payload), headers=header)
+print(response.json()['RtData']['QuoteList']
