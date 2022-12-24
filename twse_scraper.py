25865edbec1ef5718f7325539baf77c49ac963da
Eason12030105 <eason12030105@gmail.com>
2022-12-24 22:04:08 +0800

    4.4-批次爬取Jason格式的網頁資料(Post)

--- twse_scraper.py
+++ twse_scraper.py
@@ -1,9 +1,12 @@
 import requests
 import json
 
-response = requests.get('https://www.twse.com.tw/zh/exchangeReport/BWIBBU_d')
-# print(response.json()['data'][1])
+#response = requests.get('https://www.twse.com.tw/zh/exchangeReport/BWIBBU_d')
 
+response = requests.get('https://www.twse.com.tw/exchangeReport/BWIBBU_d?response=json&date=20221014&selectType=30&_=1665900731336')
+
+# print(response.json()['data'][1])
+print(response.json()['data'][0])
 print(response.json()['data'][1])
 print(response.json()['data'][2])
 print(response.json()['data'][3])
